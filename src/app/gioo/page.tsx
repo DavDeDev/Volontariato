3c1b0e10c64acff915d018bf6ab84dc1aefa3972
David Pietrocola <pietrocoladavid@gmail.com>
2024-02-24 17:44:12 -0500

    feat: adds auth

--- src/app/gioo/page.tsx
+++ src/app/gioo/page.tsx
@@ -1,23 +1,30 @@
 
 import { WavyBackground } from "@/components/ui/wavy-background";
 import Image from "next/image";
-import { UserButton } from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
-export default function Home() {
+const Home = () => {
   return (
     <main className="">
 
       <WavyBackground speed="fast" className="max-w-4xl mx-auto pb-40">
         <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
-          Volontariato (root)
+          Volontariato (Second Page)
         </p>
         <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
           Organize your volunteer trips with ease
         </p>
-        <Link className="bg-white" href="/gioo">Start now!</Link>
-
+        <SignedIn>
+        {/* Mount the UserButton component */}
+        <UserButton afterSignOutUrl='/' />
+      </SignedIn>
+      <SignedOut>
+        {/* Signed out users get sign in button */}
+        <SignInButton/>
+      </SignedOut>
       </WavyBackground>
     </main>
   );
 }
+export default Home;
